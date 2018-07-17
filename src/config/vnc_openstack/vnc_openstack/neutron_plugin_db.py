dda28c5d7919980d4e78cefe73e37dd54c9d2ad6
Petr Jediný <petr.jediny@gmail.com>
2018-07-17 13:47:02 +0200

    Tweak Jan's code to be consistent with upstream
    Change-Id: Icc54482719b873cb0b9360ba0f6d894a4e419e7b

--- src/config/vnc_openstack/vnc_openstack/neutron_plugin_db.py
+++ src/config/vnc_openstack/vnc_openstack/neutron_plugin_db.py
@@ -2172,7 +2172,7 @@ class DBInterface(object):
                 'binding:vnic_type' in port_q and port_q['binding:vnic_type'] == 'baremetal')
             if not allowed_port:
                 port_bindings = port_obj.get_virtual_machine_interface_bindings()
-                kvps = port_bindings.get_key_value_pair() if port_bindings else []
+                kvps = port_bindings.get_key_value_pair() or []
                 for kvp in kvps:
                     if kvp.key == 'host_id' and kvp.value == "null":
                         allowed_port = True
